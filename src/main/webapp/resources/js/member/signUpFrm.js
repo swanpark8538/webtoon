d05ae911492a48e36e3dd448da1a29c6afcf64e5
swanpark8538 <swanpark8538@gmail.com>
2024-06-07 15:18:31 +0900

    click이벤트

--- src/main/webapp/resources/js/member/signUpFrm.js
+++ src/main/webapp/resources/js/member/signUpFrm.js
@@ -1,29 +1,26 @@
 //아이디 제약조건 확인
-$("#checkId").keyup(function () {
-  const memberId = $(this).val();
-  const idRegex = /^[a-z0-9],{6,20}&/; //소문자(영어),숫자의 그룹으로 6~20자 사이를 뜻하는 정규표현식
-  if (idRegex.test(id)) {
+const checkId = document.getElementById("checkId");
+checkId.addEventListener("click", function () {
+  const memberId = getElementById("memberId").value;
+  const idRegex = /^[a-z0-9]{6,20}$/; //소문자(영어) 또는 숫자의 그룹으로 6~20자 사이를 뜻하는 정규표현식
+  if (idRegex.test(memberId)) {
     $.ajax({
       type: "get",
       url: "/member/checkIdIsDuplicated",
       data: { memberId: memberId },
       dataType: json,
       success: function (result) {
-        if ((result.data.message = "success")) {
-          //태그 생성
-          const div = $("<div>");
-          div.addClass("");
-          div.text("사용 가능한 아이디입니다.");
-          //태그 넣기
-          $("#memberId").parent().parent().append(div);
+        //태그 생성
+        const parentParent = document.getElementById("memberId").parentElement.parentElement;
+        const div = document.createElement("div");
+        if ((result = "success")) {
+          div.classList.add("inputMsg-success");
+          div.textContent = "사용 가능한 아이디입니다.";
         } else {
-          //태그 생성
-          const div = $("<div>");
-          div.addClass("");
-          div.text("사용 불가능한 아이디입니다.");
-          //태그 넣기
-          $("#memberId").parent().parent().append(div);
+          div.classList.add("inputMsg-error");
+          div.textContent = "사용 불가능한 아이디입니다.";
         }
+        parentParent.appendChild(div);
       },
       error: function (result) {
         console.log(result.data);
